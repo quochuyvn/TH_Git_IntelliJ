45becd808e0101c19a7a29749bdbb644a3412e88
Quoc Huy Win <quochuynguyenkhtn@gmail.com>
2026-02-03 14:48:55 +0700

    My Rebase from develop to main

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -4,6 +4,7 @@ public class FirstClass {
         System.out.println("Hello, Selenium Java!!!");
         System.out.println("Hello, Push directly from main!!!");
         System.out.println("Hello, Pull Request Develop!!!");
+        System.out.println("Delete the last commit that no push yet.");
         System.out.println("Git Stash 01");
         System.out.println("Git Rebase Main 01");
 
